9a338bd067ff04d30f44b401a03d7e2c3502a9ff
rateshsachdeva <rateshsachdeva@gmail.com>
2025-06-22 16:39:28 +0530

    Update sidebar-switcher.tsx

--- components/sidebar/sidebar-switcher.tsx
+++ components/sidebar/sidebar-switcher.tsx
@@ -15,6 +15,10 @@ import { WithTooltip } from "../ui/with-tooltip"
 import { ProfileSettings } from "../utility/profile-settings"
 import { SidebarSwitchItem } from "./sidebar-switch-item"
 
+// ================== NEW IMPORT ==================
+import { useProfile } from "../utility/profile-provider"
+// ===============================================
+
 export const SIDEBAR_ICON_SIZE = 28
 
 interface SidebarSwitcherProps {
@@ -24,6 +28,11 @@ interface SidebarSwitcherProps {
 export const SidebarSwitcher: FC<SidebarSwitcherProps> = ({
   onContentTypeChange
 }) => {
+  // ================== NEW ROLE CHECK ==================
+  const { profile } = useProfile()
+  const isAdmin = profile?.role === "admin"
+  // ====================================================
+
   return (
     <div className="flex flex-col justify-between border-r-2 pb-5">
       <TabsList className="bg-background grid h-[440px] grid-rows-7">
@@ -33,45 +42,53 @@ export const SidebarSwitcher: FC<SidebarSwitcherProps> = ({
           onContentTypeChange={onContentTypeChange}
         />
 
-        <SidebarSwitchItem
-          icon={<IconAdjustmentsHorizontal size={SIDEBAR_ICON_SIZE} />}
-          contentType="presets"
-          onContentTypeChange={onContentTypeChange}
-        />
+        {/* ================== CONDITIONAL RENDERING BLOCK ================== */}
+        {/* The following icons will only be rendered if the user is an admin. */}
+        {isAdmin && (
+          <>
+            <SidebarSwitchItem
+              icon={<IconAdjustmentsHorizontal size={SIDEBAR_ICON_SIZE} />}
+              contentType="presets"
+              onContentTypeChange={onContentTypeChange}
+            />
 
-        <SidebarSwitchItem
-          icon={<IconPencil size={SIDEBAR_ICON_SIZE} />}
-          contentType="prompts"
-          onContentTypeChange={onContentTypeChange}
-        />
+            <SidebarSwitchItem
+              icon={<IconPencil size={SIDEBAR_ICON_SIZE} />}
+              contentType="prompts"
+              onContentTypeChange={onContentTypeChange}
+            />
 
-        <SidebarSwitchItem
-          icon={<IconSparkles size={SIDEBAR_ICON_SIZE} />}
-          contentType="models"
-          onContentTypeChange={onContentTypeChange}
-        />
+            <SidebarSwitchItem
+              icon={<IconSparkles size={SIDEBAR_ICON_SIZE} />}
+              contentType="models"
+              onContentTypeChange={onContentTypeChange}
+            />
 
+            <SidebarSwitchItem
+              icon={<IconBooks size={SIDEBAR_ICON_SIZE} />}
+              contentType="collections"
+              onContentTypeChange={onContentTypeChange}
+            />
+
+            <SidebarSwitchItem
+              icon={<IconRobotFace size={SIDEBAR_ICON_SIZE} />}
+              contentType="assistants"
+              onContentTypeChange={onContentTypeChange}
+            />
+
+            <SidebarSwitchItem
+              icon={<IconBolt size={SIDEBAR_ICON_SIZE} />}
+              contentType="tools"
+              onContentTypeChange={onContentTypeChange}
+            />
+          </>
+        )}
+        {/* =============================================================== */}
+
+        {/* This "Files" icon will be visible to all users */}
         <SidebarSwitchItem
           icon={<IconFile size={SIDEBAR_ICON_SIZE} />}
           contentType="files"
-          onContentTypeChange={onContentTypeChange}
-        />
-
-        <SidebarSwitchItem
-          icon={<IconBooks size={SIDEBAR_ICON_SIZE} />}
-          contentType="collections"
-          onContentTypeChange={onContentTypeChange}
-        />
-
-        <SidebarSwitchItem
-          icon={<IconRobotFace size={SIDEBAR_ICON_SIZE} />}
-          contentType="assistants"
-          onContentTypeChange={onContentTypeChange}
-        />
-
-        <SidebarSwitchItem
-          icon={<IconBolt size={SIDEBAR_ICON_SIZE} />}
-          contentType="tools"
           onContentTypeChange={onContentTypeChange}
         />
       </TabsList>
